f74ff00e6dc2e5696d1fd9c902675bbd77a07566
zheng shuo <117711668.com>
2021-08-07 18:04:10 +0800

    完成场景搭建和Player模型和动画优化

---
+++ Assets/Scripts/NameText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameText : MonoBehaviour
+{
+    public GameObject nameText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(nameText != null)
+        {
+            nameText.transform.LookAt(Camera.main.transform.position);
+            nameText.transform.Rotate(0, 180, 0);
+        }
+
+    }
+}
