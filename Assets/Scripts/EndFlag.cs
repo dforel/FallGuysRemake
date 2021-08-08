1c72bc0b3b0efbec147853b86c7ad1a11047ec8d
zheng shuo <117711668.com>
2021-08-09 07:50:36 +0800

    完成游戏终点排行榜功能

--- Assets/Scripts/EndFlag.cs
+++ Assets/Scripts/EndFlag.cs
@@ -5,11 +5,18 @@ using UnityEngine.UI;
 
 public class EndFlag : MonoBehaviour
 {
+    public Text rankText1; //存放1-25名
+    public Text rankText2; //存放25-50名
+
+    private ArrayList rankList = new ArrayList();
+    //private string rankList1;
+    //private string rankList2;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //rankList1 = "";
+        //rankList2 = "";
     }
 
     // Update is called once per frame
@@ -22,13 +29,33 @@ public class EndFlag : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            //Debug.Log("Player到达终点");
-            //other.gameObject
+            string player = other.transform.Find("NameText").GetComponent<TextMesh>().text;
+            rankList.Add(player);
         }
 
         if (other.CompareTag("Enemy"))
         {
-            //Debug.Log("Enemy到达终点");
+            string enemy = other.transform.Find("NameText").GetComponent<TextMesh>().text;
+            rankList.Add(enemy);
+        }
+
+        if(rankList.Count <= 25)
+        {
+            string rankList1 = "";
+            for (int i = 0; i < rankList.Count; i++)
+            {
+                rankList1 += (i + 1).ToString() + "-" + rankList[i] + "\n";
+            }
+            rankText1.text = rankList1;
+        }
+        else
+        {
+            string rankList2 = "";
+            for (int i = 25; i < rankList.Count; i++)
+            {
+                rankList2 += (i + 1).ToString() + "-" + rankList[i] + "\n";
+            }
+            rankText2.text = rankList2;
         }
     }
 }
--- Assets/Scripts/NameText.cs
+++ Assets/Scripts/NameText.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NameText : MonoBehaviour
 {
@@ -9,7 +10,6 @@ public class NameText : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
