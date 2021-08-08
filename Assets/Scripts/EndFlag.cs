017405dea5954b503049ab55681b4cf2762b9997
zheng shuo <117711668.com>
2021-08-08 22:03:23 +0800

    制作终点排行榜功能ing

---
+++ Assets/Scripts/EndFlag.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndFlag : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Debug.Log("Player到达终点");
+            //other.gameObject
+        }
+
+        if (other.CompareTag("Enemy"))
+        {
+            //Debug.Log("Enemy到达终点");
+        }
+    }
+}
--- Assets/Scripts/EnemyControls.cs
+++ Assets/Scripts/EnemyControls.cs
@@ -38,6 +38,7 @@ public class EnemyControls : MonoBehaviour
     private int levelEndDoorNum;
 
     private int currentLevel;
+    private bool flag;
 
 
     // Start is called before the first frame update
@@ -55,6 +56,7 @@ public class EnemyControls : MonoBehaviour
         level7DoorNum = Random.Range(0, DoorPointsLv7.Length);
         levelEndDoorNum = Random.Range(0, EndDoorPoints.Length);
         currentLevel = 1;
+        flag = false;
     }
 
     // Update is called once per frame
@@ -64,148 +66,147 @@ public class EnemyControls : MonoBehaviour
         //moveDirection.y = 0f;
         //controller.Move(enemySpeed * moveDirection * Time.deltaTime);
 
-        isGround = controller.isGrounded; //判断Enemy是否在地面
-        if (isGround && enemyVelocity.y < 0)
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            enemyVelocity.y = 0f;
-            anim.SetBool("Jump", false);
+            flag = true;
         }
 
-        //处理Enemy下落
-        enemyVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(enemyVelocity * Time.deltaTime);
-
-        Vector3 moveDirection = Vector3.zero;
-        Vector2 enemyTrans = Vector2.zero;
-        Vector2 doorTrans = Vector2.zero;
-        float dis = 10000f;
-
-        switch (currentLevel)
+        if(flag)
         {
-            case 1:
-                moveDirection = (DoorPointsLv1[level1DoorNum].position - transform.position).normalized;
-                moveDirection.y = 0f;
-                Run(moveDirection);
-                RandomJump();
-                enemyTrans = new Vector2(transform.position.x, transform.position.z);
-                doorTrans = new Vector2(DoorPointsLv1[level1DoorNum].position.x, DoorPointsLv1[level1DoorNum].position.z);
-                dis = (enemyTrans - doorTrans).sqrMagnitude;
-                Debug.Log("dis " + dis);
-                if (dis < minDistance)
-                {
-                    currentLevel = 2;
-                    Jump();
-                }
-                break;
-            case 2:
-                moveDirection = (DoorPointsLv2[level2DoorNum].position - transform.position).normalized;
-                moveDirection.y = 0f;
-                Run(moveDirection);
-                RandomJump();
-                enemyTrans = new Vector2(transform.position.x, transform.position.z);
-                doorTrans = new Vector2(DoorPointsLv2[level2DoorNum].position.x, DoorPointsLv2[level2DoorNum].position.z);
-                dis = (enemyTrans - doorTrans).sqrMagnitude;
-                Debug.Log("dis " + dis);
-                if (dis < minDistance)
-                {
-                    currentLevel = 3;
-                    Jump();
-                }
-                break;
-            case 3:
-                moveDirection = (DoorPointsLv3[level3DoorNum].position - transform.position).normalized;
-                moveDirection.y = 0f;
-                Run(moveDirection);
-                RandomJump();
-                enemyTrans = new Vector2(transform.position.x, transform.position.z);
-                doorTrans = new Vector2(DoorPointsLv3[level3DoorNum].position.x, DoorPointsLv3[level3DoorNum].position.z);
-                dis = (enemyTrans - doorTrans).sqrMagnitude;
-                Debug.Log("dis " + dis);
-                if (dis < minDistance)
-                {
-                    currentLevel = 4;
-                    Jump();
-                }
-                break;
-            case 4:
-                moveDirection = (DoorPointsLv4[level4DoorNum].position - transform.position).normalized;
-                moveDirection.y = 0f;
-                Run(moveDirection);
-                RandomJump();
-                enemyTrans = new Vector2(transform.position.x, transform.position.z);
-                doorTrans = new Vector2(DoorPointsLv4[level4DoorNum].position.x, DoorPointsLv4[level4DoorNum].position.z);
-                dis = (enemyTrans - doorTrans).sqrMagnitude;
-                Debug.Log("dis " + dis);
-                if (dis < minDistance)
-                {
-                    currentLevel = 5;
-                    Jump();
-                }
-                break;
-            case 5:
-                moveDirection = (DoorPointsLv5[level5DoorNum].position - transform.position).normalized;
-                moveDirection.y = 0f;
-                Run(moveDirection);
-                RandomJump();
-                enemyTrans = new Vector2(transform.position.x, transform.position.z);
-                doorTrans = new Vector2(DoorPointsLv5[level5DoorNum].position.x, DoorPointsLv5[level5DoorNum].position.z);
-                dis = (enemyTrans - doorTrans).sqrMagnitude;
-                Debug.Log("dis " + dis);
-                if (dis < minDistance)
-                {
-                    currentLevel = 6;
-                    Jump();
-                }
-                break;
-            case 6:
-                moveDirection = (DoorPointsLv6[level6DoorNum].position - transform.position).normalized;
-                moveDirection.y = 0f;
-                Run(moveDirection);
-                RandomJump();
-                enemyTrans = new Vector2(transform.position.x, transform.position.z);
-                doorTrans = new Vector2(DoorPointsLv6[level6DoorNum].position.x, DoorPointsLv6[level6DoorNum].position.z);
-                dis = (enemyTrans - doorTrans).sqrMagnitude;
-                Debug.Log("dis " + dis);
-                if (dis < minDistance)
-                {
-                    currentLevel = 7;
-                    Jump();
-                }
-                break;
-            case 7:
-                moveDirection = (DoorPointsLv7[level7DoorNum].position - transform.position).normalized;
-                moveDirection.y = 0f;
-                Run(moveDirection);
-                RandomJump();
-                enemyTrans = new Vector2(transform.position.x, transform.position.z);
-                doorTrans = new Vector2(DoorPointsLv7[level7DoorNum].position.x, DoorPointsLv7[level7DoorNum].position.z);
-                dis = (enemyTrans - doorTrans).sqrMagnitude;
-                Debug.Log("dis " + dis);
-                if (dis < minDistance)
-                {
-                    currentLevel = 8;
-                    Jump();
-                }
-                break;
-            case 8:
-                moveDirection = (EndDoorPoints[levelEndDoorNum].position - transform.position).normalized;
-                moveDirection.y = 0f;
-                Run(moveDirection);
-                RandomJump();
-                enemyTrans = new Vector2(transform.position.x, transform.position.z);
-                doorTrans = new Vector2(EndDoorPoints[levelEndDoorNum].position.x, EndDoorPoints[levelEndDoorNum].position.z);
-                dis = (enemyTrans - doorTrans).sqrMagnitude;
-                Debug.Log("dis " + dis);
-                if (dis < minDistance)
-                {
-                    currentLevel = 9;
-                    Debug.Log("到达终点啦！");
-                    anim.SetBool("Run", false);
-                }
-                break;
-            default:
-                break;
-        }
+            isGround = controller.isGrounded; //判断Enemy是否在地面
+            if (isGround && enemyVelocity.y < 0)
+            {
+                enemyVelocity.y = 0f;
+                anim.SetBool("Jump", false);
+            }
+
+            //处理Enemy下落
+            enemyVelocity.y += gravityValue * Time.deltaTime;
+            controller.Move(enemyVelocity * Time.deltaTime);
+
+            Vector3 moveDirection = Vector3.zero;
+            Vector2 enemyTrans = Vector2.zero;
+            Vector2 doorTrans = Vector2.zero;
+            float dis = 10000f;
+
+            switch (currentLevel)
+            {
+                case 1:
+                    moveDirection = (DoorPointsLv1[level1DoorNum].position - transform.position).normalized;
+                    moveDirection.y = 0f;
+                    Run(moveDirection);
+                    RandomJump();
+                    enemyTrans = new Vector2(transform.position.x, transform.position.z);
+                    doorTrans = new Vector2(DoorPointsLv1[level1DoorNum].position.x, DoorPointsLv1[level1DoorNum].position.z);
+                    dis = (enemyTrans - doorTrans).sqrMagnitude;
+                    if (dis < minDistance)
+                    {
+                        currentLevel = 2;
+                        Jump();
+                    }
+                    break;
+                case 2:
+                    moveDirection = (DoorPointsLv2[level2DoorNum].position - transform.position).normalized;
+                    moveDirection.y = 0f;
+                    Run(moveDirection);
+                    RandomJump();
+                    enemyTrans = new Vector2(transform.position.x, transform.position.z);
+                    doorTrans = new Vector2(DoorPointsLv2[level2DoorNum].position.x, DoorPointsLv2[level2DoorNum].position.z);
+                    dis = (enemyTrans - doorTrans).sqrMagnitude;
+                    if (dis < minDistance)
+                    {
+                        currentLevel = 3;
+                        Jump();
+                    }
+                    break;
+                case 3:
+                    moveDirection = (DoorPointsLv3[level3DoorNum].position - transform.position).normalized;
+                    moveDirection.y = 0f;
+                    Run(moveDirection);
+                    RandomJump();
+                    enemyTrans = new Vector2(transform.position.x, transform.position.z);
+                    doorTrans = new Vector2(DoorPointsLv3[level3DoorNum].position.x, DoorPointsLv3[level3DoorNum].position.z);
+                    dis = (enemyTrans - doorTrans).sqrMagnitude;
+                    if (dis < minDistance)
+                    {
+                        currentLevel = 4;
+                        Jump();
+                    }
+                    break;
+                case 4:
+                    moveDirection = (DoorPointsLv4[level4DoorNum].position - transform.position).normalized;
+                    moveDirection.y = 0f;
+                    Run(moveDirection);
+                    RandomJump();
+                    enemyTrans = new Vector2(transform.position.x, transform.position.z);
+                    doorTrans = new Vector2(DoorPointsLv4[level4DoorNum].position.x, DoorPointsLv4[level4DoorNum].position.z);
+                    dis = (enemyTrans - doorTrans).sqrMagnitude;
+                    if (dis < minDistance)
+                    {
+                        currentLevel = 5;
+                        Jump();
+                    }
+                    break;
+                case 5:
+                    moveDirection = (DoorPointsLv5[level5DoorNum].position - transform.position).normalized;
+                    moveDirection.y = 0f;
+                    Run(moveDirection);
+                    RandomJump();
+                    enemyTrans = new Vector2(transform.position.x, transform.position.z);
+                    doorTrans = new Vector2(DoorPointsLv5[level5DoorNum].position.x, DoorPointsLv5[level5DoorNum].position.z);
+                    dis = (enemyTrans - doorTrans).sqrMagnitude;
+                    if (dis < minDistance)
+                    {
+                        currentLevel = 6;
+                        Jump();
+                    }
+                    break;
+                case 6:
+                    moveDirection = (DoorPointsLv6[level6DoorNum].position - transform.position).normalized;
+                    moveDirection.y = 0f;
+                    Run(moveDirection);
+                    RandomJump();
+                    enemyTrans = new Vector2(transform.position.x, transform.position.z);
+                    doorTrans = new Vector2(DoorPointsLv6[level6DoorNum].position.x, DoorPointsLv6[level6DoorNum].position.z);
+                    dis = (enemyTrans - doorTrans).sqrMagnitude;
+                    if (dis < minDistance)
+                    {
+                        currentLevel = 7;
+                        Jump();
+                    }
+                    break;
+                case 7:
+                    moveDirection = (DoorPointsLv7[level7DoorNum].position - transform.position).normalized;
+                    moveDirection.y = 0f;
+                    Run(moveDirection);
+                    RandomJump();
+                    enemyTrans = new Vector2(transform.position.x, transform.position.z);
+                    doorTrans = new Vector2(DoorPointsLv7[level7DoorNum].position.x, DoorPointsLv7[level7DoorNum].position.z);
+                    dis = (enemyTrans - doorTrans).sqrMagnitude;
+                    if (dis < minDistance)
+                    {
+                        currentLevel = 8;
+                        Jump();
+                    }
+                    break;
+                case 8:
+                    moveDirection = (EndDoorPoints[levelEndDoorNum].position - transform.position).normalized;
+                    moveDirection.y = 0f;
+                    Run(moveDirection);
+                    RandomJump();
+                    enemyTrans = new Vector2(transform.position.x, transform.position.z);
+                    doorTrans = new Vector2(EndDoorPoints[levelEndDoorNum].position.x, EndDoorPoints[levelEndDoorNum].position.z);
+                    dis = (enemyTrans - doorTrans).sqrMagnitude;
+                    if (dis < minDistance)
+                    {
+                        currentLevel = 9;
+                        anim.SetBool("Run", false);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }        
     }
 
     void Run(Vector3 direction)
